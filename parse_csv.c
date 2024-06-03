450eec2aa09ac4138fedcc9c86c8ef684e6c51b3
Thibaut Le Goff <thibaut.le.goff27@gmail.com>
2024-06-03 21:50:27 +0200

    modifié :         parse_csv.c 	modifié :         run.sh

--- parse_csv.c
+++ parse_csv.c
@@ -8,6 +8,52 @@
 
 // from https://github.com/portfoliocourses/c-example-code/blob/main/csv_to_struct_array.c
 
+/*
+
+typedef struct 
+{
+    int lines_number;
+    char *columns_names;
+    char *columns_data_type;
+    
+    float *float_values_to_change;
+    float *float_values_of_replacement;
+
+    char *char_values_to_change_in_int;
+    float *char_to_int_values_of_replacement;
+
+} CSV_info
+
+
+typedef struct
+{
+    char *column_name;
+    char **column_data;
+
+    Float_column_node *next_float_node;
+    Char_column_node *next_char_node;
+
+} Char_column_node
+
+
+typedef struct
+{
+    char *column_name;
+    float *column_data;
+
+    Float_column_node *next_float_node;
+    Char_column_node *next_char_node;
+
+} Float_column_node
+
+
+Char_column_node *csv_to_linked_list_char(CSV_info *csv_raw_info, FILE* csv_raw) {
+    
+
+}
+
+*/
+
 typedef struct 
 {
     int passenger_id[NUMBER_RECORDS];
@@ -26,16 +72,11 @@ typedef struct
 } CSV_Passangers;
 
 
-void missing_value_case(CSV_Passangers *passengers, int *records_index, char *line);
-void missing_value_replacement(CSV_Passangers *passengers, int *records_index, int *missing_value_indicator, char *value_assigned);
+int read_line(CSV_Passangers *passengers, int *records_index, char *line);
+int value_assignation(CSV_Passangers *passengers, int *records_index, int *missing_value_indicator, char *value_assigned);
 
 int main()
 {
-
-    char *data_types[] = {"passenger_id", "survived", "pclass", 
-                          "surname", "forename", "sex", "age", 
-                          "sibsp", "parch", "ticket", "fare", 
-                          "cabin", "embarked"}; 
 
     FILE *file = fopen("titanic_csv/train.csv", "r");
     
@@ -52,52 +93,23 @@ int main()
     char line[MAX_LINE_LENGTH];
 
 
+    if (fgets(line, sizeof(line), file) != NULL) {}
+
+
     while (fgets(line, sizeof(line), file) != NULL)
     {
-        //int result_sscanf = sscanf(line,
-          //         "%d, ",
-            //       &passengers[records_index].passenger_id, 
-        /*
-        int result_sscanf = sscanf(line,
-                   "%d, %d, %d, %99[^,], %99[^,], %9[^,], %d, %d, %d, %9[^,], %lf, %9[^,], %c\n",
-            //       "%d, %d, %49[^,], %49[^,], %9[^,], %d, %d, %d, %9[^,], %lf, %9[^,], %c\n",
-                   &passengers->passenger_id[records_index], 
-                   &passengers->survived[records_index], 
-                   &passengers->pclass[records_index],
-                   passengers->surname[records_index],
-                   passengers->forename[records_index],
-                   passengers->sex[records_index], 
-                   &passengers->age[records_index], 
-                   &passengers->sibsp[records_index], 
-                   &passengers->parch[records_index], 
-                   passengers->ticket[records_index],
-                   &passengers->fare[records_index],
-                   passengers->cabin[records_index],
-                   &passengers->embarked[records_index]); 
-                
-        if (result_sscanf == 13)
-        {
-            records_index++;
-        }
-        else
-        {
-            printf("Error reading line: %s, the %s is missing\n", line, data_types[result_sscanf]);
-            
-            //missing_value_case(&result_sscanf, line);           
-            missing_value_case(passengers, &records_index, line);           
-            
+        int read_line_result = read_line(passengers, &records_index, line);
+
+        if (read_line_result == 1) {
+            //return 1;
             records_index_failed++;
-        }
-        */
-        missing_value_case(passengers, &records_index, line);
+        }   
         records_index++;
     }
 
     fclose(file);
   
-    //records_index = 4;
-
-    for (int record = 1; record < records_index; record++) {
+    for (int record = 0; record < records_index; record++) {
         printf("%d, %d, %d, %s, %s, %s, %d, %d, %d, %s, %lf, %s, %c\n",
                 passengers->passenger_id[record], 
                 passengers->survived[record], 
@@ -118,12 +130,13 @@ int main()
 
     printf("\n%d records_index read.\n\n", records_index);
     printf("\n%d records_index failed.\n\n", records_index_failed);
+    
     return 0;
 }
 
 
-//Passanger *missing_value_case(char *line) {
-void missing_value_case(CSV_Passangers *passengers, int *records_index, char *line) {
+//Passanger *read_line(char *line) {
+int read_line(CSV_Passangers *passengers, int *records_index, char *line) {
     printf("dans la fonction : %s\n", line);
 
 
@@ -134,11 +147,9 @@ void missing_value_case(CSV_Passangers *passengers, int *records_index, char *li
 
    char *data_missing_values[] = {"-1", "-1", "-1", 
                                  "<N/A>", "<N/A>", "<N/A>", "-1.0", 
-                                 //"<N/A>", "<N/A>", "<N/A>", "float(FLT_MIN)", 
                                  "-1", "-1", "<N/A>", "-1.0", 
                                  "<N/A>", "N"}; 
     
-   //char *data_type[] = {"%d, %d, %d, %99[^,], %99[^,], %9[^,], %d, %d, %d, %9[^,], %lf, %9[^,], %c\n"};
 
     // from https://stackoverflow.com/questions/4917030/move-cursor-x-lines-from-current-position-in-vi-vim
     char *delimiter = ",";
@@ -151,31 +162,34 @@ void missing_value_case(CSV_Passangers *passengers, int *records_index, char *li
         if (*separated_data == '\0') {
             printf("the %s is missing, id must be remplaced by %s\n", data_types[delimiter_counter], data_missing_values[delimiter_counter]);
 
-            //missing_value_replacement = 
-            missing_value_replacement(passengers, records_index, &delimiter_counter, data_missing_values[delimiter_counter]);
+            //value_assignation = 
+            int result_assignation = value_assignation(passengers, records_index, &delimiter_counter, data_missing_values[delimiter_counter]);
+
+            if (result_assignation == 0) {
+                return 1;
+            }
         }
         else {
             printf("the %s is %s\n", data_types[delimiter_counter], separated_data);
-            missing_value_replacement(passengers, records_index, &delimiter_counter, separated_data);
+            int result_assignation = value_assignation(passengers, records_index, &delimiter_counter, separated_data);
+            
+            if (result_assignation == 0) {
+                return 1;
+            }
         }
 
         delimiter_counter++;
     }
+
+    return 0;
 }
 
 
 
-void missing_value_replacement(CSV_Passangers *passengers, int *records_index, int *missing_value_indicator, char *value_assigned) {
-
-   /*     
-   char *data_missing_values[] = {"sqrt(-1)\n", "sqrt(-1)\n", "sqrt(-1)\n", 
-                                 "<N/A>\n", "<N/A>\n", "<N/A>\n", ", sqrt(-1)\n", 
-                                 "sqrt(-1)\n", "sqrt(-1)\n", "<N/A>\n", "sqrt(-1)\n", 
-                                 "<N/A>\n", "<N/A>\n"}; 
-    */
+int value_assignation(CSV_Passangers *passengers, int *records_index, int *missing_value_indicator, char *value_assigned) {
 
     char *data_type[] = {"%d", "%d", "%d", "%99[^,]", "%99[^,]", "%9[^,]", "%d", "%d", "%d", "%9[^,]", "%lf", "%9[^,]", "%c\n"};
-    int result_sscanf = 0;
+    int result_sscanf;
 
     switch(*missing_value_indicator) {
         case 0:
@@ -242,6 +256,9 @@ void missing_value_replacement(CSV_Passangers *passengers, int *records_index, i
             result_sscanf = sscanf(value_assigned, 
                                         data_type[*missing_value_indicator], 
                                         &passengers->embarked[*records_index]);
-            break;              
+            break;  
+            
     } 
+
+        return result_sscanf;            
 }
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 !#/bin/bash
 
-gcc parse_csv.c -o parse_csv
+# don't work with -std=c99 because strsep is not a c standard
+gcc -std=gnu99 -g -Wall -Wextra -pedantic-errors -fstrict-aliasing -fstack-protector-all -fPIE -Werror -Wfatal-errors parse_csv.c -o parse_csv
 valgrind --track-origins=yes --leak-check=full --show-leak-kinds=all -s ./parse_csv 
